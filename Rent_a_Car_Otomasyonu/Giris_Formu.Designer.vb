28e1bc86bb602bc5c5e9e1f20ceba5413e36a95a
OmerYilmaz <OmerYilmaz@DESKTOP-FPIED59>
2025-05-06 12:46:39 +0300

    giris frm

--- Rent_a_Car_Otomasyonu/Giris_Formu.Designer.vb
+++ Rent_a_Car_Otomasyonu/Giris_Formu.Designer.vb
@@ -22,8 +22,7 @@ Partial Class Giris_Formu
     'Kod düzenleyicisini kullanarak değiştirmeyin.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Giris_Formu))
         Me.CheckBox1 = New System.Windows.Forms.CheckBox()
         Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
         Me.SifreLabel = New System.Windows.Forms.Label()
@@ -31,40 +30,16 @@ Partial Class Giris_Formu
         Me.PTextBox = New System.Windows.Forms.TextBox()
         Me.KGTextBox = New System.Windows.Forms.TextBox()
         Me.GirisButton = New System.Windows.Forms.Button()
-        Me.GroupBox1.SuspendLayout()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
-        '
-        'GroupBox1
-        '
-        Me.GroupBox1.BackColor = System.Drawing.Color.Silver
-        Me.GroupBox1.Controls.Add(Me.Button1)
-        Me.GroupBox1.Controls.Add(Me.CheckBox1)
-        Me.GroupBox1.Controls.Add(Me.LinkLabel1)
-        Me.GroupBox1.Controls.Add(Me.SifreLabel)
-        Me.GroupBox1.Controls.Add(Me.KullaniciLabel)
-        Me.GroupBox1.Controls.Add(Me.PTextBox)
-        Me.GroupBox1.Controls.Add(Me.KGTextBox)
-        Me.GroupBox1.Controls.Add(Me.GirisButton)
-        Me.GroupBox1.Location = New System.Drawing.Point(234, 112)
-        Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(305, 250)
-        Me.GroupBox1.TabIndex = 0
-        Me.GroupBox1.TabStop = False
-        Me.GroupBox1.Text = "Giriş Paneli"
-        '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(7, 105)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 7
-        Me.Button1.Text = "abcC"
-        Me.Button1.UseVisualStyleBackColor = True
         '
         'CheckBox1
         '
         Me.CheckBox1.AutoSize = True
-        Me.CheckBox1.Location = New System.Drawing.Point(162, 126)
+        Me.CheckBox1.Location = New System.Drawing.Point(176, 258)
         Me.CheckBox1.Name = "CheckBox1"
         Me.CheckBox1.Size = New System.Drawing.Size(98, 20)
         Me.CheckBox1.TabIndex = 6
@@ -73,7 +48,7 @@ Partial Class Giris_Formu
         '
         'LinkLabel1
         '
-        Me.LinkLabel1.Location = New System.Drawing.Point(77, 209)
+        Me.LinkLabel1.Location = New System.Drawing.Point(147, 281)
         Me.LinkLabel1.Name = "LinkLabel1"
         Me.LinkLabel1.Size = New System.Drawing.Size(145, 23)
         Me.LinkLabel1.TabIndex = 5
@@ -84,7 +59,7 @@ Partial Class Giris_Formu
         'SifreLabel
         '
         Me.SifreLabel.AutoSize = True
-        Me.SifreLabel.Location = New System.Drawing.Point(87, 90)
+        Me.SifreLabel.Location = New System.Drawing.Point(101, 91)
         Me.SifreLabel.Name = "SifreLabel"
         Me.SifreLabel.Size = New System.Drawing.Size(50, 16)
         Me.SifreLabel.TabIndex = 4
@@ -93,7 +68,8 @@ Partial Class Giris_Formu
         'KullaniciLabel
         '
         Me.KullaniciLabel.AutoSize = True
-        Me.KullaniciLabel.Location = New System.Drawing.Point(55, 38)
+        Me.KullaniciLabel.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(230, Byte), Integer), CType(CType(233, Byte), Integer))
+        Me.KullaniciLabel.Location = New System.Drawing.Point(78, 39)
         Me.KullaniciLabel.Name = "KullaniciLabel"
         Me.KullaniciLabel.Size = New System.Drawing.Size(82, 16)
         Me.KullaniciLabel.TabIndex = 3
@@ -101,42 +77,68 @@ Partial Class Giris_Formu
         '
         'PTextBox
         '
-        Me.PTextBox.Location = New System.Drawing.Point(162, 87)
+        Me.PTextBox.Location = New System.Drawing.Point(176, 88)
         Me.PTextBox.Name = "PTextBox"
         Me.PTextBox.Size = New System.Drawing.Size(100, 22)
         Me.PTextBox.TabIndex = 2
         '
         'KGTextBox
         '
-        Me.KGTextBox.Location = New System.Drawing.Point(162, 35)
+        Me.KGTextBox.Location = New System.Drawing.Point(176, 36)
         Me.KGTextBox.Name = "KGTextBox"
         Me.KGTextBox.Size = New System.Drawing.Size(100, 22)
         Me.KGTextBox.TabIndex = 1
         '
         'GirisButton
         '
-        Me.GirisButton.Location = New System.Drawing.Point(77, 152)
+        Me.GirisButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.GirisButton.Location = New System.Drawing.Point(6, 258)
         Me.GirisButton.Name = "GirisButton"
-        Me.GirisButton.Size = New System.Drawing.Size(145, 33)
+        Me.GirisButton.Size = New System.Drawing.Size(145, 46)
         Me.GirisButton.TabIndex = 0
         Me.GirisButton.Text = "Giriş"
         Me.GirisButton.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(18, Byte), Integer), CType(CType(51, Byte), Integer), CType(CType(63, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.GirisButton)
+        Me.Panel1.Controls.Add(Me.CheckBox1)
+        Me.Panel1.Controls.Add(Me.LinkLabel1)
+        Me.Panel1.Controls.Add(Me.SifreLabel)
+        Me.Panel1.Controls.Add(Me.PTextBox)
+        Me.Panel1.Controls.Add(Me.KullaniciLabel)
+        Me.Panel1.Controls.Add(Me.KGTextBox)
+        Me.Panel1.Location = New System.Drawing.Point(456, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(341, 376)
+        Me.Panel1.TabIndex = 2
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(-2, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(357, 208)
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
         '
         'Giris_Formu
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(8, Byte), Integer), CType(CType(14, Byte), Integer), CType(CType(51, Byte), Integer))
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Panel1)
         Me.Name = "Giris_Formu"
         Me.Text = "Rent a Car Giriş Paneli"
-        Me.GroupBox1.ResumeLayout(False)
-        Me.GroupBox1.PerformLayout()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
-
-    Friend WithEvents GroupBox1 As GroupBox
     Friend WithEvents SifreLabel As Label
     Friend WithEvents KullaniciLabel As Label
     Friend WithEvents PTextBox As TextBox
@@ -144,5 +146,6 @@ Partial Class Giris_Formu
     Friend WithEvents GirisButton As Button
     Friend WithEvents CheckBox1 As CheckBox
     Friend WithEvents LinkLabel1 As LinkLabel
-    Friend WithEvents Button1 As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents PictureBox1 As PictureBox
 End Class
--- Rent_a_Car_Otomasyonu/Giris_Formu.vb
+++ Rent_a_Car_Otomasyonu/Giris_Formu.vb
@@ -42,4 +42,20 @@ Public Class Giris_Formu
 
 
     End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub PictureBox1_Click_1(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
 End Class
